2365d7ebf3a1e3bbbdd04f2446a68ec6fdb2130b
tapishsharma <tapishmail@gmail.com>
2022-02-21 20:50:04 +0530

    Updates

---
+++ precache-manifest.e9ce8be646e89587b444b3a12c9a3ba0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d028f5d3ab30680f778862a2f96aa6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91d234bb0c42c50948af",
+    "url": "/static/css/main.c3a492c3.chunk.css"
+  },
+  {
+    "revision": "4fb35bf0295d84d81ec5",
+    "url": "/static/js/2.500783fd.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.500783fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a178e01a15f2867c4c6",
+    "url": "/static/js/3.13d7147a.chunk.js"
+  },
+  {
+    "revision": "91d234bb0c42c50948af",
+    "url": "/static/js/main.037aeb34.chunk.js"
+  },
+  {
+    "revision": "dac1f2258e733f3feb83",
+    "url": "/static/js/runtime-main.2f6b437d.js"
+  }
+]);
